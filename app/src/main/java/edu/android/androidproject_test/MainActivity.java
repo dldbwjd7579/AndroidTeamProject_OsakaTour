c51edb88daee0f05d1378762d767111d08407d4d
dldbwjd7579 <dldbwjd7579@gmail.com>
2017-09-14 12:09:46 +0900

    test

---
+++ app/src/main/java/edu/android/androidproject_test/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.android.androidproject_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test(){
+        Log.i("test", "git test 입니다");
+        //
+    }
+}
